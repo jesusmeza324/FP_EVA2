47ac9d288635c31a2a14cb0e1adb73992b7a7692
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2024-10-16 16:50:13 -0600

    Add files via upload

---
+++ EVA2_8_VALIDAR/src/main/java/com/mycompany/eva2_8_validar/EVA2_8_VALIDAR.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_8_validar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA2_8_VALIDAR {
+
+    public static void main(String[] args) {
+        int dia;
+        Scanner cap=new Scanner(System.in);
+        do{
+        System.out.println("numero correspondiente al dia(1-7)");
+        dia = cap.nextInt();
+        }while(dia<1|| dia>7);{
+        switch(dia){
+            case 1:
+                System.out.println("lunes");
+                break;//ultima instruccion 
+                case 2:
+                System.out.println("martes");
+                break;
+                case 3:
+                System.out.println("miercoles");
+                break;
+                case 4:
+                System.out.println("jueves");
+                break;
+                case 5:
+                System.out.println("viernes");
+                break;
+                case 6:
+                System.out.println("sabado");
+                break;
+                case 7:
+                System.out.println("domingo");
+                break;
+                
+                
+        }
+        
+    }
+    }
+}
