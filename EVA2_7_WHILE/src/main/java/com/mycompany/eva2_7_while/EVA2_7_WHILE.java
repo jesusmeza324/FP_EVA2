d058afb0e4acd0ebbc34175a03329e855424336e
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2024-10-15 19:24:53 -0600

    Add files via upload

---
+++ EVA2_7_WHILE/src/main/java/com/mycompany/eva2_7_while/EVA2_7_WHILE.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_7_while;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA2_7_WHILE{
+    public static void main(String[] args) {
+       int i=1;
+        while (i <= 10) {
+            System.out.print(i + "-"); 
+            i++; 
+        }System.out.println("");
+        i=1;
+        do {
+            System.out.print(i + "-"); 
+            i++; 
+        } while (i <= 10); 
+    }
+}
+
