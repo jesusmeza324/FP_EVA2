16b5044a3272698deeeaeb8e29f7d26f4683609d
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2024-10-18 16:51:36 -0600

    Add files via upload

---
+++ EVA2_12_TIPOS_DATOS/src/main/java/com/mycompany/eva2_12_tipos_datos/EVA2_12_TIPOS_DATOS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_12_tipos_datos;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA2_12_TIPOS_DATOS {
+
+    public static void main(String[] args) {
+        byte valor=-128;
+        byte valor2=127;
+        System.out.println("valor=" + valor);
+        System.out.println("valor2=" + valor2);
+        short val=-32768;
+        short val2=32767;
+        int x=-2147483618;
+        int y=2147483617;
+        
+        
+        
+        
+    }
+}
