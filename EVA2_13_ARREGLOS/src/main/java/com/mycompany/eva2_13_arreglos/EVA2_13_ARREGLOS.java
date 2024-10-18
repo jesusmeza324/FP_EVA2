7e5bfa09b7a299963d21bd3911ea6b288ad3c20d
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2024-10-18 16:51:50 -0600

    Add files via upload

---
+++ EVA2_13_ARREGLOS/src/main/java/com/mycompany/eva2_13_arreglos/EVA2_13_ARREGLOS.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_13_arreglos;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA2_13_ARREGLOS {
+
+    public static void main(String[] args) {
+        int[]arreglo =new int[10];//arreglo---son variables
+        arreglo[0]=100;//ponemos 100 en el entero en la pocision 0
+        arreglo[9]=900;//ultima pocision (10-1)
+        System.out.println(arreglo[0]);
+        System.out.println(arreglo[1]);
+        System.out.println(arreglo[2]);
+        System.out.println(arreglo[3]);
+        System.out.println(arreglo[4]);
+        System.out.println(arreglo[5]);
+        System.out.println(arreglo[6]);
+        System.out.println(arreglo[7]);
+        System.out.println(arreglo[8]);
+        System.out.println(arreglo[9]);
+        System.out.println(arreglo[10]);
+        
+    }
+}
