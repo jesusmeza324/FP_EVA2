a103f6f57e98f922c06b6a1dd01d3a6428ca3da0
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2024-10-22 16:07:50 -0600

    Add files via upload

---
+++ EVA2_16_ARREGLOS/src/main/java/com/mycompany/eva2_16_arreglos/EVA2_16_ARREGLOS.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_16_arreglos;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA2_16_ARREGLOS {
+
+    public static void main(String[] args) {
+        String[] diasSemana ={"domingo", "lunes", "martes", "miercoles", "jueves","viernes","sabado"};
+        for (int i = 0; i < diasSemana.length; i++) {
+            System.out.print(diasSemana[i]+"-");
+        }
+    }
+}
