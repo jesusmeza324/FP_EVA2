1134bc468615882ce0ebd26c20a4e950004d3474
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2024-10-22 16:06:41 -0600

    Add files via upload

---
+++ EVA2_15_ARREGLOS/src/main/java/com/mycompany/eva2_15_arreglos/EVA2_15_ARREGLOS.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_15_arreglos;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA2_15_ARREGLOS {
+
+    public static void main(String[] args) {
+        String[] diasSemana =new String [7];
+        diasSemana[0]="domingo";
+        diasSemana[1]="lunes";
+        diasSemana[2]="martes";
+        diasSemana[3]="miercoles";
+        diasSemana[4]="jueves";
+        diasSemana[5]="viernes";
+        diasSemana[6]="sabado";
+        for (int i = 0; i < diasSemana.length; i++) {
+            System.out.print(diasSemana[i] +"-");
+            
+        }
+        
+    }
+}
